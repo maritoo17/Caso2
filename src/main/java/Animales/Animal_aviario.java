f07b5cb1ff26e73b97b054e9d600417bad67b77d
maritoo17 <msancgim@myuax.com>
2024-03-12 18:56:14 +0100

    19

--- src/main/java/Animales/Animal_aviario.java
+++ src/main/java/Animales/Animal_aviario.java
@@ -7,10 +7,7 @@ public class Animal_aviario extends Animal {
     boolean plumaje;
     String tipo_aviario;
 
-    public Animal_aviario(String nombre, String especie, String raza, String color, String sexo, String edad,
-                          String peso, String altura, String tipo, String habitat, String alimentacion, String reproduccion,
-                          String cuidados, String enfermedades, String vacunas, int id, boolean salud, boolean comportamiento,  boolean vuela, boolean pico,
-                          boolean plumaje, String tipo_aviario) {
+    public Animal_aviario(String nombre, String especie, String raza, String color, String sexo, String edad, double peso, String altura, String tipo, String habitat, String alimentacion, String reproduccion, String cuidados, String enfermedades, String vacunas, int id, boolean salud, boolean comportamiento,  boolean vuela, boolean pico, boolean plumaje, String tipo_aviario) {
         super(nombre, especie, raza, color, sexo, edad, peso, altura, tipo, habitat, alimentacion, reproduccion, cuidados, enfermedades, vacunas, comportamiento, salud, id);
         this.vuela = vuela;
         this.pico = pico;
