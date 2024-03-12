3592f0098c06033b0f4bc538d14839ca9cc719dd
maritoo17 <msancgim@myuax.com>
2024-03-12 18:44:39 +0100

    15

---
+++ src/main/java/Visitantes/Trabajador.java
@@ -0,0 +1,27 @@
+package Visitantes;
+
+public class Trabajador {
+    String nombre;
+    String puesto;
+
+    public Trabajador(String nombre, String puesto) {
+        this.nombre = nombre;
+        this.puesto = puesto;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+}
